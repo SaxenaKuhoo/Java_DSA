930aa2b4fc79293371d3fb913aa2b6dd1e838290
Kuhoo Saxena <92252901+SaxenaKuhoo@users.noreply.github.com>
2022-01-13 23:43:40 +0530

    Update Dupele.java

--- src/Array/Dupele.java
+++ src/Array/Dupele.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 //Q. Write a program to find whether the array of integers contains a duplicate number.
 // if yes print the location where duplicate numbers are found.
 
-public class dupele {
+public class Dupele {
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
